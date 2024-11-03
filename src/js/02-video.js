125e3d95104cae07fff9b9ba2eda47bcd8b3dc34
User <96388713+aura80@users.noreply.github.com>
2024-11-03 14:30:26 +0200

    HW 8 JS ex 2 + 3 added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeElement = document.querySelector('#vimeo-player');
+const player = new Player(iframeElement);
+
+console.log(player);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+// retrieve time from the local storage
+const timeFromLS = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+// resumes playing from where it left off last time
+if (timeFromLS) {
+  // time from the last stop becomes the current time + error handling
+  player.setCurrentTime(timeFromLS).catch(error => {
+    alert('Current time not set!');
+    console.error('Current time not set: ', error);
+  });
+}
+
+// update the current time to local storage at most once per second
+// event.seconds captures the exact time the video is
+player.on(
+  'timeupdate',
+  throttle(
+    event => localStorage.setItem(LOCAL_STORAGE_KEY, event.seconds),
+    1000
+  ),
+  console.log('Current time: ', localStorage.getItem(LOCAL_STORAGE_KEY))
+);
+
+// checks
+player.on('play', () => {
+  console.log('Video playing!');
+});
+
+player.on('pause', () => {
+  console.log('Video paused!');
+  console.log(localStorage.getItem(LOCAL_STORAGE_KEY));
+});
+
+player.on('ended', () => {
+  console.log('Video ended!');
+  console.log(localStorage.getItem(LOCAL_STORAGE_KEY));
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const formElement = document.querySelector('.feedback-form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+// initializes form fields with values from local storage
+const loadForm = () => {
+  const elemLS = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  if (elemLS) {
+    const objLS = JSON.parse(elemLS);
+
+    formElement.elements.email.value = objLS.email || '';
+    formElement.elements.message.value = objLS.message || '';
+  }
+};
+
+loadForm();
+
+// save form to local storage once at 500 ms
+// throttledFunction created once before event listener - one instance is reused for each input event
+// throttledFunction remains the same for all input events
+const throttledFunction = throttle(() => {
+  const formObj = {
+    email: formElement.elements.email.value,
+    message: formElement.elements.message.value,
+  };
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formObj));
+}, 500);
+
+formElement.addEventListener('input', throttledFunction);
+
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+
+  console.log({
+    email: formElement.elements.email.value,
+    message: formElement.elements.message.value,
+  });
+
+  // removes the item from the local storage
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+
+  // reset the form
+  //   formElement.reset();
+});
